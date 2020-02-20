880049548b8cef27dfc4147319c37668b4c63d4e
vigneshraja-g <vigneshraja20@gmail.com>
2020-02-20 07:59:22 +0530

    Updates

---
+++ precache-manifest.477af1988eb3b8565be6222530c56d1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b17bd41bcfccae41467d2b1f3928ab",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7a3d41cec3cf6c8d2380",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ad8e8dccf92391a62d32",
+    "url": "/portfolio/static/js/2.50fdd86e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.50fdd86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3d41cec3cf6c8d2380",
+    "url": "/portfolio/static/js/main.3c521b97.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
